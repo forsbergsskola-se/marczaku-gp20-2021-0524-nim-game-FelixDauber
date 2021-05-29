39f2938d2d529dcae4c49f2609c99c7d4357171d
FelixDauber <71184613+FelixDauber@users.noreply.github.com>
2021-05-29 19:22:28 +0200

    o crap didn't push this

--- tic-tac-toe/tic-tac-toe/tic-tac-toe.cpp
+++ tic-tac-toe/tic-tac-toe/tic-tac-toe.cpp
@@ -2,10 +2,157 @@
 //
 
 #include <iostream>
+using namespace std;
+
+class game
+{
+public:
+    int squares[9] = {0,0,0,0,0,0,0,0,0};
+    bool player2turn = false;
+    int playerWon = 0;
+
+    void startGame() {
+        while (true)
+        {
+            while (playerWon == 0) {
+                doRound();
+            }
+            displayGameField();
+            displayWinner();
+            if (!rematch()) break;
+            resetGame();
+        }
+    }
+
+    void resetGame() {
+        for (int i = 0; i < 9; i++)
+        {
+            squares[i] = 0;
+        }
+        playerWon = 0;
+    }
+   
+    bool rematch() {
+        int i = 0;
+        cout << "Rematch? 0=No, 1=Yes";
+        cin >> i;
+        if (i == 0) {
+            return false;
+        }
+        if (i == 1) {
+            return true;
+        }
+        return rematch();
+    }
+
+    void doRound() {
+        displayGameField();
+        doInput(); 
+        checkForWin();
+    }
+
+    void displayGameField() {
+        for (int i = 0; i < 9; i++)
+        {
+            displayIcon(squares[i]);
+            if (i == 2 || i == 5) {
+                std::cout << endl;
+            }
+        }
+        std::cout << endl;
+    }
+
+    void displayIcon(int i) {
+        if (i == 1)
+        {
+            std::cout << "X";
+        }
+        else if (i == 2)
+        {
+            std::cout << "O";
+        }
+        else
+        {
+            std::cout << "#";
+        }
+    }
+
+    void doInput() {
+        int x = 0, y = 0;
+        cout << "Input X cordinate: ";
+        cin >> x;
+
+        cout << "Input Y cordinate: ";
+        cin >> y;
+
+        if (checkIfValidCordinate(x, y)) {
+            cout << "SquarePos " << x << " " << y <<  " " << getSquareOnPosition(x, y) << endl;
+            squares[getSquareOnPosition(x,y)] = player2turn + 1;
+            player2turn = !player2turn;
+        }
+    }
+
+    bool checkIfInside(int i) {
+        return (i > 0 && i < 4);
+    }
+
+    bool checkIfValidCordinate(int x, int y) {
+        return checkIfInside(x) && checkIfInside(y) && checkIfSquareAvailable(x, y);
+    }
+
+    int getSquareOnPosition(int x, int y) {
+        int returnValue = x - 1;
+        returnValue += (y - 1) * 3;
+        return returnValue;
+    }
+
+    bool checkIfSquareAvailable(int x, int y) {
+        return squares[getSquareOnPosition(x, y)] == 0;
+    }
+
+    bool checkForWin() {
+        if (checkWinningRows())
+        {
+            playerWon = !player2turn + 1;
+            return true;
+        }
+        return false;
+    }
+
+    bool checkWinningRows()
+    {
+        return (
+            //Horizontal
+            checkThreeSquares(0, 1, 2) ||
+            checkThreeSquares(3, 4, 5) ||
+            checkThreeSquares(6, 7, 8) ||
+
+            //Vertical
+            checkThreeSquares(0, 3, 6) ||
+            checkThreeSquares(1, 4, 7) ||
+            checkThreeSquares(2, 5, 8) ||
+
+            //Diagonal
+            checkThreeSquares(0, 4, 8) ||
+            checkThreeSquares(2, 4, 6)
+            );
+    }
+
+    bool checkThreeSquares(int one, int two, int three)
+    {
+        return (squares[one] != 0 && squares[one] == squares[two] && squares[two] == squares[three]);
+    }
+
+    void displayWinner() {
+        cout << "Player " << !player2turn + 1 << " has won!";
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Welcome to Tic-Tac-Toe!\n\n";
+    game currentGame;
+    currentGame.startGame();
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
